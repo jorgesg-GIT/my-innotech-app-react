fe6e7855c503ca28771e1031040db0c3452d5544
Jorge Sanchez Gonzalez <j.b.sanchez.gonzalez@accenture.com>
2024-03-07 19:49:16 +0100

    Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   test: {
+    environment: "jsdom",
     coverage: {
-      thresholds: {
-        lines: 60,
-        branches: 60,
-        functions: 60,
-        statements: 60
-      }
-    }
-  }
+      reporter: ["text", "json", "json-summary"],
+    },
+    setupFiles: ["./test/setup.ts"],
+  },
 });
