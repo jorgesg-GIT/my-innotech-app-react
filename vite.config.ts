753a8cb59282c151a1b195884f23c021cb3c2e18
Jorge Sanchez Gonzalez <j.b.sanchez.gonzalez@accenture.com>
2024-03-07 19:54:02 +0100

    Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
   test: {
-    environment: "jsdom",
     coverage: {
-      reporter: ["text", "json", "json-summary"],
-    },
-  },
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
 });
